28accdd426a3bcaff2897ebd168e66d19007ae9e
shaikhazam990 <shaikhazam0990@gmail.com>
2026-04-01 18:31:14 +0530

    implement graph

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,15 @@
-import { config } from "dotenv";
-config();
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // GEMINI_API_KEY
 // MISTRAL_API_KEY
 // COHERE_API_KEY
 
 type CONFIG = {
-    readonly GOOGLE_API_KEY:String;
-    readonly MISTRAL_API_KEY:String;
-    readonly COHERE_API_KEY:String;
+    readonly GOOGLE_API_KEY:string;
+    readonly MISTRAL_API_KEY:string;
+    readonly COHERE_API_KEY:string;
 }
 
 const config: CONFIG={
--- src/services/models.service.ts
+++ src/services/models.service.ts
@@ -1,10 +1,11 @@
-import { ChatGoogle } from "@langchain/google";
+// import { ChatGoogle } from "@langchain/google";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { ChatCohere } from "@langchain/cohere";
 import config from "../config/config.js";
 
-export const geminiModel = new ChatGoogle({
-    model:"gemini-flash-latest",
+export const geminiModel = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-flash",
     apiKey: config.GOOGLE_API_KEY
 
 });
